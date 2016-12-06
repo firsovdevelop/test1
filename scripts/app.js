7c22ef0ed6cddd15c19688ecb517c0f78ddd53e8
firsovdevelop <firsov.develop@gmail.com>
2016-12-06 15:32:46 +0300

    Добавление interceptor

--- scripts/app.js
+++ scripts/app.js
@@ -17,9 +17,34 @@ var myApp = angular.module('myApp', ['ngRoute'])
 	}]);
 
 myApp.controller('pageCtrl', ['$scope', '$ngRoute', function($scope, ngRoute) {	
-    	
-    
+        
 	var self = this;
 	
+}]);
+
+// Эксперимент Interceptor 
+myApp.factory('MyAuthInterceptor', ['$window', '$q', '$log', function($window, $q, $log) {
+        $log.debug('Начало авторизации');		
+        return {
+            request: function(config) {
+                config.headers = config.headers || {};
+                if ($window.localStorage.getItem('token')) {
+					// Подстановка значения token
+                    config.headers.Authorization = 'token ' + $window.localStorage.getItem('token');
+                }				
+                return config || $q.when(config);
+            },
+            response: function(response) {
+                if (response.status === 401) {
+                    $log.debug('Пользователь не авторизован!');
+                }
+                return response || $q.when(response);
+            }
+        };
+    }
+]);
+
+myApp.config(['$httpProvider', function($httpProvider) {      
+        $httpProvider.interceptors.push('MyAuthInterceptor');
 }]);
    
