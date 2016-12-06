d820ab6c99dbb8d4250cfea401e6050f2860aff6
firsovdevelop <firsov.develop@gmail.com>
2016-12-06 17:52:47 +0300

    Пробная авторизация через API

--- scripts/app.js
+++ scripts/app.js
@@ -5,38 +5,56 @@ var myApp = angular.module('myApp', ['ngRoute'])
 		$routeProvider
 		.when('/login',
 		{
-			templateUrl:'views/login.html'
+			templateUrl:'views/login.html'			
 		})
 		.when('/secure',
 		{
-			templateUrl:'views/secure.html'
+			templateUrl:'views/secure.html'			
 		})
 		.otherwise({
-			redirectTo: '/'
+			redirectTo: '/'			
 		});
 	}]);
 
-myApp.controller('pageCtrl', ['$scope', '$ngRoute', function($scope, ngRoute) {	
+myApp.controller('pageCtrl', ['$scope', function($scope) {	
         
 	var self = this;
 	
+	// Объект для работы с API вконтакте
+	this.VK = {};
+	
+	// Авторизация
+	this.VK.login = function(){
+		VK.Auth.login(null, VK.access.FRIENDS);
+	};
+	
 }]);
+
+// Модуль Авторизации вконтакте
+
 
 // Эксперимент Interceptor 
 myApp.factory('MyAuthInterceptor', ['$window', '$q', '$log', function($window, $q, $log) {
-        $log.debug('Начало авторизации');		
+        $log.debug('Начало авторизации');
+		$log.debug($window.localStorage);
         return {
             request: function(config) {
 				// Перед каждым запросом добавляем token
                 config.headers = config.headers || {};
+				/*
+					Проверять VK.Auth.getLoginStatus перед каждым запросом					
+					для авторизованных пользователей показывать страницу secter
+					для неавторизованных показывать страницу login						
+				*/
                 if ($window.localStorage.getItem('token')) {
-					// Подстановка значения token
+					// Подстановка значения token					
                     config.headers.Authorization = 'token ' + $window.localStorage.getItem('token');
-                }				
+                }		
                 return config || $q.when(config);
             },
             response: function(response) {
 				// Если ответ "плохой", сообщаем что пользователь не авторизован
+				// Перенаправление на страницу login
                 if (response.status === 401) {
                     $log.debug('Пользователь не авторизован!');
                 }
@@ -49,4 +67,5 @@ myApp.factory('MyAuthInterceptor', ['$window', '$q', '$log', function($window, $
 myApp.config(['$httpProvider', function($httpProvider) {      
         $httpProvider.interceptors.push('MyAuthInterceptor');
 }]);
+
    
